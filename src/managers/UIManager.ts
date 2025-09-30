fc2c514326eb1c5d0d51bf8590c7d57c5d8d81e8
Dariusz Steblik <dariusz.steblik@grandparade.co.uk>
2025-09-30 16:36:54 +0200

    [GAME-005] Refactor GameScene file and improve object model

---
+++ src/managers/UIManager.ts
@@ -0,0 +1,68 @@
+import Phaser from "phaser";
+
+export class UIManager {
+    private scene: Phaser.Scene;
+    private scoreText!: Phaser.GameObjects.Text;
+    private livesText!: Phaser.GameObjects.Text;
+
+    constructor(scene: Phaser.Scene) {
+        this.scene = scene;
+
+        this.scene = scene;
+
+        // @ts-ignore
+        this.scoreText = this.scene.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' }).setScrollFactor(0);
+        // @ts-ignore
+        this.livesText = this.scene.add.text(16, 48, 'Lives: 3', { fontSize: '24px', fill: '#fff' }).setScrollFactor(0);
+
+        this.scene.events.on('scoreChanged', this.updateScore, this);
+        this.scene.events.on('livesChanged', this.updateLives, this);
+        this.scene.events.on('gameOver', this.showGameOver, this);
+
+        this.scene.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
+            this.scene.events.off('scoreChanged', this.updateScore, this);
+            this.scene.events.off('livesChanged', this.updateLives, this);
+            this.scene.events.off('gameOver', this.showGameOver, this);
+        });
+    }
+
+    private updateScore = (score: number) => {
+        this.scoreText.setText(`Score: ${score}`);
+    }
+
+    private updateLives = (lives: number) => {
+        this.livesText.setText(`Lives: ${lives}`);
+    }
+
+
+    showStartScreen = (onStart: () => void) => {
+        const overlay = this.scene.add.rectangle(this.scene.scale.width / 2, this.scene.scale.height / 2, this.scene.scale.width, this.scene.scale.height, 0x00000, 0.6).setDepth(100);
+        const title = this.scene.add.text(this.scene.scale.width / 2, this.scene.scale.height / 2 - 80, 'First Adventure', { fontSize: '40px', color: '#ffffff' }).setOrigin(0.5).setDepth(101);
+        const startText = this.scene.add.text(this.scene.scale.width / 2, this.scene.scale.height / 2 - 10, 'Press ENTER to start', { fontSize: '24px', color: '#ffffff'}).setOrigin(0.5).setDepth(101);
+        const helpText = this.scene.add.text(this.scene.scale.width / 2, this.scene.scale.height / 2 + 40, 'Arrows to move | SHIFT to run', { fontSize: '18px', color: '#dddddd'}).setOrigin(0.5).setDepth(101);
+
+        // @ts-ignore
+        const enterKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+        enterKey.once('down', () => {
+            overlay.destroy();
+            title.destroy();
+            startText.destroy();
+            helpText.destroy();
+            onStart();
+        });
+    }
+
+    private showGameOver = () => {
+        const weight = this.scene.scale.width / 2;
+        const height = this.scene.scale.height / 2;
+        this.scene.add.rectangle(weight, height, this.scene.scale.width, this.scene.scale.height, 0x00000, 0.7).setDepth(200);
+        this.scene.add.text(weight, height - 30, 'Game Over', { fontSize: '36px', color: '#fff' }).setOrigin(0.5).setDepth(201);
+        this.scene.add.text(weight, height + 20, 'Press ENTER to restart', { fontSize: '20px', color: '#fff' }).setOrigin(0.5).setDepth(201);
+
+        // @ts-ignore
+        const enter = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+        enter.once('down', () => {
+            this.scene.scene.restart();
+        });
+    }
+}
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,196 +1,120 @@
 import Phaser from 'phaser';
 import { Enemy } from '../characters/Enemy';
 import Player from '../characters/Player';
-
-let player: Player;
-let coins: Phaser.Physics.Arcade.Group;
-let score = 0;
-let scoreText: Phaser.GameObjects.Text;
-let lives = 3;
-let livesText: Phaser.GameObjects.Text;
-let gameStarted = false;
-let isPlayerInvulnerable = false;
-let cursors: Phaser.Types.Input.Keyboard.CursorKeys;
-let shiftKey: Phaser.Input.Keyboard.Key;
-let enemies: Phaser.Physics.Arcade.Group;
+import { UIManager } from '../managers/UIManager.ts';
+import { GameManager } from '../managers/GameManager.ts';
 
 const PLAYER_SPAWN = { x: 100, y: 300 };
 
-export function preload(this: Phaser.Scene) {
-    this.load.image('coin', '/assets/goldStar.png');
-    this.load.spritesheet('adventurer', '/assets/adventurer.png', {
-        frameWidth: 80,
-        frameHeight: 112
-    });
-    this.load.spritesheet('enemy', '/assets/enemy.png', {
-        frameWidth: 80,
-        frameHeight: 112
-    })
-}
+export class GameScene extends Phaser.Scene {
 
-// TODO: to refactor
-export function create(this: Phaser.Scene) {
-    player = new Player(this, PLAYER_SPAWN.x, PLAYER_SPAWN.y);
+    private player!: Player;
+    private coins!: Phaser.Physics.Arcade.Group;
+    private enemies!: Phaser.Physics.Arcade.Group;
+    private shiftKey!: Phaser.Input.Keyboard.Key;
+    private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
 
-    coins = this.physics.add.group();
-    for (let i = 0; i < 12; i++) {
-        const x = Phaser.Math.Between(50, 750);
-        const y = Phaser.Math.Between(50, 550);
-        const coin = coins.create(x, y, 'coin') as Phaser.Physics.Arcade.Image;
-        coin.setScale(0.5);
+    private ui!: UIManager;
+    private gameManager!: GameManager;
+
+    constructor() {
+        super({ key: 'GameScene' });
     }
 
-    enemies = this.physics.add.group();
-    for (let i = 0; i < 4; i++) {
-        const x = Phaser.Math.Between(100, 700);
-        const y = Phaser.Math.Between(100, 500);
-        const enemy = new Enemy(this, x, y, 'enemy');
-
-        enemy.minX = x - Phaser.Math.Between(50, 150);
-        enemy.maxX = x + Phaser.Math.Between(50, 150);
-
-        enemies.add(enemy);
-    }
-
-    cursors = this.input.keyboard?.createCursorKeys() as Phaser.Types.Input.Keyboard.CursorKeys;
-    // @ts-ignore
-    shiftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
-
-    if (!this.anims.exists('walk')) {
-        this.anims.create({
-            key: 'walk',
-            frames: this.anims.generateFrameNumbers('adventurer', { start: 9, end: 10 }),
-            frameRate: 6,
-            repeat: -1,
+    preload = ()=> {
+        this.load.image('coin', '/assets/goldStar.png');
+        this.load.spritesheet('adventurer', '/assets/adventurer.png', {
+            frameWidth: 80,
+            frameHeight: 112
+        });
+        this.load.spritesheet('enemy', '/assets/enemy.png', {
+            frameWidth: 80,
+            frameHeight: 112
         });
     }
 
-    if (!this.anims.exists('run')) {
-        this.anims.create({
-            key: 'run',
-            frames: this.anims.generateFrameNumbers('adventurer', { start: 9, end: 10 }),
-            frameRate: 10,
-            repeat: -1,
-        });
-    }
+    create = ()=> {
+        this.gameManager = new GameManager(this);
+        this.ui = new UIManager(this);
 
-    if (!this.anims.exists('idle')) {
-        this.anims.create({
-            key: 'idle',
-            frames: [{ key: 'adventurer', frame: 0 }]
-        });
-    }
+        this.player = new Player(this, PLAYER_SPAWN.x, PLAYER_SPAWN.y);
 
-    player.play('idle');
-
-    if (!this.anims.exists('enemyWalk')) {
-        this.anims.create({
-            key: 'enemyWalk',
-            frames: this.anims.generateFrameNumbers('enemy', { start: 9, end: 10 }),
-            frameRate: 6,
-            repeat: -1,
-        });
-    }
-
-    // @ts-ignore
-    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
-    // @ts-ignore
-    livesText = this.add.text(16, 48, `Lives: ${lives}`, {fontSize: '24px', fill: '#fff'});
-
-    const overlay = this.add.rectangle(this.scale.width / 2, this.scale.height / 2, this.scale.width, this.scale.height, 0x00000, 0.6).setDepth(100);
-    const title = this.add.text(this.scale.width / 2, this.scale.height / 2 - 80, 'First Adventure', { fontSize: '40px', color: '#ffffff' }).setOrigin(0.5).setDepth(101);
-    const startText = this.add.text(this.scale.width / 2, this.scale.height / 2 - 10, 'Press ENTER to start', { fontSize: '24px', color: '#ffffff'}).setOrigin(0.5).setDepth(101);
-    const helpText = this.add.text(this.scale.width / 2, this.scale.height / 2 + 40, 'Arrows to move | SHIFT to run', { fontSize: '18px', color: '#dddddd'}).setOrigin(0.5).setDepth(101);
-
-    // @ts-ignore
-    const enterKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
-    enterKey.once('down', () => {
-        overlay.destroy();
-        title.destroy();
-        startText.destroy();
-        helpText.destroy();
-        gameStarted = true;
-    });
-
-    this.input.once('pointerdown', () => {
-        if(!gameStarted) {
-            overlay.destroy();
-            title.destroy();
-            startText.destroy();
-            helpText.destroy();
-            gameStarted = true;
+        this.coins = this.physics.add.group();
+        for (let i = 0; i < 12; i++) {
+            const x = Phaser.Math.Between(50, 750);
+            const y = Phaser.Math.Between(50, 550);
+            const coin = this.coins.create(x, y, 'coin') as Phaser.Physics.Arcade.Image;
+            coin.setScale(0.5);
         }
-    })
 
-    // @ts-ignore
-    this.physics.add.overlap(player, coins, collectCoin, undefined, this);
-    // @ts-ignore
-    this.physics.add.collider(player, enemies, hitEnemy, undefined, this);
-}
+        this.enemies = this.physics.add.group();
+        for (let i = 0; i < 4; i++) {
+            const x = Phaser.Math.Between(100, 700);
+            const y = Phaser.Math.Between(100, 500);
+            const enemy = new Enemy(this, x, y, 'enemy');
+            enemy.minX = x - Phaser.Math.Between(50, 150);
+            enemy.maxX = x + Phaser.Math.Between(50, 150);
+            this.enemies.add(enemy);
+        }
 
-// @ts-ignore
-function collectCoin(playerObj: Phaser.GameObjects.GameObject, coinObj: Phaser.GameObjects.GameObject) {
-    const coin = coinObj as Phaser.Physics.Arcade.Image;
-    coin.disableBody(true, true);
-    score += 10;
-    scoreText.setText(`Score: ${score}`);
-}
+        this.cursors = this.input.keyboard?.createCursorKeys() as Phaser.Types.Input.Keyboard.CursorKeys;
+        // @ts-ignore
+        this.shiftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT);
 
-// TODO: to refactor
-// @ts-ignore
-function hitEnemy(this: Phaser.Scene, playerObj: Phaser.GameObjects.GameObject, enemyObj: Phaser.GameObjects.GameObject) {
-    if(!gameStarted) return;
-    if(isPlayerInvulnerable) return;
+        this.setupAnimations();
 
-    lives -= 1;
-    livesText.setText(`Lives: ${lives}`);
-
-    const player = playerObj as Phaser.Physics.Arcade.Sprite;
-    const enemy = enemyObj as Phaser.Physics.Arcade.Sprite;
-
-    if (player && enemy && player.body) {
-        const dx = player.x - enemy.x;
-        const dy = player.y - enemy.y;
-        const angle = Math.atan2(dy, dx);
-        const force = 300;
-        player.setVelocity(Math.cos(angle) * force, Math.sin(angle) * force);
-    }
-
-    isPlayerInvulnerable = true;
-    player.setTint(0xff0000);
-
-    this.time.delayedCall(1000, () => {
-        isPlayerInvulnerable = false;
-        player.clearTint();
-    });
-
-    if(lives <= 0) {
-        const weight = this.scale.width / 2;
-        const height = this.scale.height / 2;
-        this.add.rectangle(weight, height, this.scale.width, this.scale.height, 0x00000, 0.7).setDepth(200);
-        this.add.text(weight, height - 30, 'Game Over', { fontSize: '36px', color: '#fff' }).setOrigin(0.5).setDepth(201);
-        this.add.text(weight, height + 20, 'Press ENTER to restart', { fontSize: '20px', color: '#fff' }).setOrigin(0.5).setDepth(201);
+        this.player.play('idle');
 
         // @ts-ignore
-        const enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
-        enter.once('down', () => {
-            score = 0;
-            lives = 3;
-            this.scene.restart();
+        this.physics.add.overlap(this.player, this.coins, this.gameManager.collectCoin, undefined, this.gameManager);
+        // @ts-ignore
+        this.physics.add.collider(this.player, this.enemies, this.gameManager.hitEnemy, undefined, this.gameManager);
+
+        this.ui.showStartScreen(() => {
+            this.gameManager.startGame();
         });
     }
-}
 
-export function update(this: Phaser.Scene) {
-    if(gameStarted) {
-        if (player) player.move(cursors, shiftKey);
-        (enemies.getChildren() as Enemy[]).forEach((enemy) => {
-            enemy.update();
-        })
-    }else {
-        if (player) {
-            player.setVelocity(0, 0);
-            player.anims.play('idle', true);
+    update() {
+        if (this.gameManager.isStarted) {
+            this.player.move(this.cursors, this.shiftKey);
+            (this.enemies.getChildren() as Enemy[]).forEach(enemy => enemy.update());
+        } else {
+            this.player.setVelocity(0, 0);
+            this.player.anims.play('idle', true);
+        }
+    }
+
+    private setupAnimations() {
+        if (!this.anims.exists('walk')) {
+            this.anims.create({
+                key: 'walk',
+                frames: this.anims.generateFrameNumbers('adventurer', { start: 9, end: 10 }),
+                frameRate: 6,
+                repeat: -1,
+            });
+        }
+        if (!this.anims.exists('run')) {
+            this.anims.create({
+                key: 'run',
+                frames: this.anims.generateFrameNumbers('adventurer', { start: 9, end: 10 }),
+                frameRate: 10,
+                repeat: -1,
+            });
+        }
+        if (!this.anims.exists('idle')) {
+            this.anims.create({
+                key: 'idle',
+                frames: [{ key: 'adventurer', frame: 0 }]
+            });
+        }
+        if (!this.anims.exists('enemyWalk')) {
+            this.anims.create({
+                key: 'enemyWalk',
+                frames: this.anims.generateFrameNumbers('enemy', { start: 9, end: 10 }),
+                frameRate: 6,
+                repeat: -1,
+            });
         }
     }
 }
